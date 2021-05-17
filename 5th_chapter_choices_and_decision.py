50f0e314d632061c3a303213583cc0dde73c07ef
Abhishek Kumar <abhibuddyofficial@gmail.com>
2021-05-17 13:00:16 +0530

    Renamed Files

---
+++ 5th_chapter_choices_and_decision.py
@@ -0,0 +1,14 @@
+#conditional statements
+print(5!=2)
+print(5>2)
+print(2<5)
+print(2<=5)
+print(2>=2)
+print(2==2)
+userInput=input("please enter 1 or 2 :")
+if userInput=="1":
+    print(" Hello How are you ? you just entered 1 ")
+elif userInput=="2":
+    print(" Python Rocks, You just entered 2 ")
+else :
+    print("print enter a valid number")
